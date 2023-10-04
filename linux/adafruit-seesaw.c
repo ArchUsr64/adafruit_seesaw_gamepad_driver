9999ba8c7539742787a8237e38e37608cd53017e
Anshul Dalal <anshulusr@gmail.com>
2023-10-04 19:05:29 +0530

    moved to `module_i2c_driver` for initialization

--- linux/adafruit-seesaw.c
+++ linux/adafruit-seesaw.c
@@ -2,7 +2,7 @@
 #include <linux/i2c.h>
 #include <linux/delay.h>
 
-#define SEESAW_DEVICE_NAME "Adafruit Seesaw"
+#define SEESAW_DEVICE_NAME "seesaw_gamepad"
 
 // Base registers
 #define SEESAW_STATUS_BASE 0
@@ -43,14 +43,12 @@ u32 BUTTON_MASK = (1UL << BUTTON_X) | (1UL << BUTTON_Y) |
 
 #define I2C_AVAILABLE_BUS 1
 
-static struct i2c_adapter *adapter = NULL;
-static struct i2c_client *seesaw = NULL;
-static const struct i2c_device_id seesaw_id[] = { { SEESAW_DEVICE_NAME, 0 },
-						  {} };
-MODULE_DEVICE_TABLE(i2c, seesaw_id);
+struct seesaw_data {
+	__be16 x;
+	__be16 y;
+	u8 button_x, button_y, button_a, button_b, button_select, button_start;
+};
 
-static struct i2c_board_info seesaw_info = { I2C_BOARD_INFO(
-	SEESAW_DEVICE_NAME, SEESAW_I2C_ADDRESS) };
 
 static unsigned char read(struct i2c_client *client)
 {
@@ -62,6 +60,9 @@ static unsigned char read(struct i2c_client *client)
 // Called once the device has been found on the i2c adapter
 static int seesaw_probe(struct i2c_client *client)
 {
+	if (client->addr != SEESAW_I2C_ADDRESS) {
+		pr_info("Invalid Address: %d\n", client->addr);
+	}
 	// Software reset the registers
 	{
 		unsigned char buf[] = { 0x00, 0x7F, 0xFF };
@@ -159,43 +160,35 @@ static int seesaw_probe(struct i2c_client *client)
 	return 0;
 }
 
-static void seesaw_remove(struct i2c_client *client)
-{
-	pr_info("Device removed!");
-}
+#ifdef CONFIG_OF
+static const struct of_device_id of_seesaw_match[] = {
+	{
+		.compatible = "adafruit,seesaw_gamepad",
+	},
+	{},
+};
+MODULE_DEVICE_TABLE(of, of_seesaw_match);
+#endif /* CONFIG_OF */
+
+static const struct i2c_device_id seesaw_id_table[] = { { SEESAW_DEVICE_NAME,
+							  0 },
+							{} };
+MODULE_DEVICE_TABLE(i2c, seesaw_id_table);
 
 static struct i2c_driver seesaw_driver = {
 	.driver = {
-	.name = SEESAW_DEVICE_NAME,
-	.owner = THIS_MODULE,
-},
+		.name = SEESAW_DEVICE_NAME,
+		.of_match_table = of_match_ptr(of_seesaw_match),
+		.owner = THIS_MODULE,
+	},
+	.id_table = seesaw_id_table,
 // For testing with the Raspberry Pi on kernel v6.1
 #ifndef _ARCH_X86_TLBBATCH_H
 	.probe_new	= seesaw_probe,
 #else
 	.probe		= seesaw_probe,
 #endif
-	.remove = seesaw_remove,
-	.id_table = seesaw_id,
 };
+module_i2c_driver(seesaw_driver);
 
-static int __init seesaw_driver_init(void)
-{
-	pr_info("Module Started\n");
-	adapter = i2c_get_adapter(I2C_AVAILABLE_BUS);
-	seesaw = i2c_new_client_device(adapter, &seesaw_info);
-	i2c_add_driver(&seesaw_driver);
-	return 0;
-}
-
-static void __exit seesaw_driver_exit(void)
-{
-	i2c_unregister_device(seesaw);
-	i2c_del_driver(&seesaw_driver);
-
-	pr_info("Module Exitted!\n");
-}
-
-module_init(seesaw_driver_init);
-module_exit(seesaw_driver_exit);
 MODULE_LICENSE("GPL v2");
